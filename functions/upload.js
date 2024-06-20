4ff1f9c8cd085ac38a518b3f1a77b768302e5e5c
gzx1988 <iamgzx@163.com>
2024-06-20 11:27:15 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -14,7 +14,7 @@ export async function onRequestPost(context) {  // Contents of context object
      const url = new URL(request.url);
     
     if (decodeURIComponent(params.password) != 'gzx.10086') {
-        return new UnauthorizedException(request.headers.Origin)
+       return new Response("ok", { headers });
     }
    
      const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
